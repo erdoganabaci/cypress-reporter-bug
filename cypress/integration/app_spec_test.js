50cdf0fa85be620f9f4612a4d6746d05cf77f96f
erdoganabaci <erdoganabaci97@gmail.com>
2022-05-14 11:23:19 +0200

    first commit

---
+++ cypress/integration/app_spec_test.js
@@ -0,0 +1,79 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../support" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+// ***********************************************
+// All of these tests are written to implement
+// the official TodoMVC tests written for Selenium.
+//
+// The Cypress tests cover the exact same functionality,
+// and match the same test names as TodoMVC.
+// Please read our getting started guide
+// https://on.cypress.io/introduction-to-cypress
+//
+// You can find the original TodoMVC tests here:
+// https://github.com/tastejs/todomvc/blob/master/tests/test.js
+// ***********************************************
+
+describe('TodoMVC - React', function () {
+  // setup these constants to match what TodoMVC does
+  let TODO_ITEM_ONE = 'buy some cheese'
+  let TODO_ITEM_TWO = 'feed the cat'
+  let TODO_ITEM_THREE = 'book a doctors appointment'
+
+  beforeEach(function () {
+    // By default Cypress will automatically
+    // clear the Local Storage prior to each
+    // test which ensures no todos carry over
+    // between tests.
+    //
+    // Go out and visit our local web server
+    // before each test, which serves us the
+    // TodoMVC App we want to test against
+    //
+    // We've set our baseUrl to be http://localhost:8888
+    // which is automatically prepended to cy.visit
+    //
+    // https://on.cypress.io/api/visit
+    cy.visit('/')
+  })
+
+  afterEach(() => {
+    // In firefox, blur handlers will fire upon navigation if there is an activeElement.
+    // Since todos are updated on blur after editing,
+    // this is needed to blur activeElement after each test to prevent state leakage between tests.
+    cy.window().then((win) => {
+      // @ts-ignore
+      win.document.activeElement.blur()
+    })
+  })
+
+  // a very simple example helpful during presentations
+  it('adds 2 todos', function () {
+    cy.get('.new-todo')
+    .type('learn testing{enter}')
+    .type('be cool{enter}')
+
+    cy.get('.todo-list li').should('have.length', 2)
+  })
+
+  context('When page is initially opened', function () {
+    it('should focus on the todo input field', function () {
+      // get the currently focused element and assert
+      // that it has class='new-todo'
+      //
+      // http://on.cypress.io/focused
+      cy.focused().should('have.class', 'new-todo')
+    })
+  })
+
+
+
+    
+  
+})
---
+++ cypress/plugin/index.js
@@ -0,0 +1,3 @@
+module.exports = (on, config) => {
+    require('cypress-mochawesome-reporter/plugin')(on);
+  };
